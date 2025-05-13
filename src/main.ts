a863a7b126751ac3fd1e44a647bb23a2b107e8f4
lukesjc <162274275+lukesjc@users.noreply.github.com>
2025-05-13 15:39:21 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import router from './router';
-
 // Create the Pinia store instance
 const pinia = createPinia();
-
 // Create the Vue app instance
 const app = createApp(App);
-
 // Use Pinia for state management
 app.use(pinia);
-
 // Use Vue Router for routing
 app.use(router);
-
 // Mount the app to the DOM
 app.mount('#app');
